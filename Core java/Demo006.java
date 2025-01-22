21cd5780c63a9d9c04aea8d4cc24d3e306099291
Sandeep-patel-17 <chikotisandeep71@gmail.com>
2025-01-22 12:22:25 +0530

    Diff Username & Password

---
+++ Core java/Demo006.java
@@ -0,0 +1,14 @@
+public class Demo006 {
+  public static void main(String[] args){
+    String name = "rohit sharma";
+    String password = "hitman";
+    while((name == "rohit sharma") && (password=="hitman")){
+        System.out.println("Hello user Happy New Year");
+        break;
+
+    }
+   
+  }  
+}
+
+
